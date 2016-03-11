0e03c488008c60c198e7141f9db92590a00c4d36
Peerapat Jankhaow <peerapat40595@Peerapats-MacBook-Pro.local>
2016-03-11 10:48:21 +0700

    phpmyadminFile

---
+++ config.inc.php
@@ -0,0 +1,14 @@
+<?php
+$cfg['blowfish_secret'] = 'ba17c1ec07d65012';  // use here a value of your choice
+$i = 0;
+/* First server */
+$i++;
+/* Authentication type */
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+/* Server parameters */
+$cfg['Servers'][$i]['host'] = 'ap-cdbr-azure-southeast-b.cloudapp.net';  // Replace with value from connection string
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['extension'] = 'mysqli';
+$cfg['Servers'][$i]['AllowNoPassword'] = false;
+?>
